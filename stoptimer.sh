c155b2d712b2262e341ae8afdce70ff4942f4b3c
taibhse <taibhse@users.noreply.github.com>
2014-09-14 01:21:58 +0100

    Create stoptimer.sh

---
+++ stoptimer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo 'Collecting Epoch for shutdown'
+DOWN=$(date +%s)
+
+echo 'Reading provided file for start Epoch time'
+filename="$1"
+while read line
+do
+    DIFF=$(($DOWN-$line))
+    echo 'Outputting Difference of start and stop Epoch values to uptimer-seconds, with a date'
+    echo $DIFF 1>> uptimer-seconds.log && date +%F 1>> uptimer-seconds.log
+done < $filename
