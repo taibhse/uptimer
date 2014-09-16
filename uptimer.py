344c74cdb29d3041466ecb53260adf810c425f8e
taibhse <chris@redbrick.dcu.ie>
2014-09-16 23:52:38 +0100

    further readme modification, some minor edits and thinking

--- uptimer.py
+++ uptimer.py
@@ -2,8 +2,11 @@
 import sys
 
 def main():
+    print len(sys.argv)
     with open(sys.argv[1]) as f:#this should open uptimer-start.log
         for line in f:
+            print line
+            line = line.strip()
             print line
 
 if __name__=="__main__":
