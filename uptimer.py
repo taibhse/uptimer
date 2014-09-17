c9c1bd1f72a2b30333e95b61309b5d265967b1e3
Chris Walsh <cwalsh@newrelic.com>
2014-09-17 11:42:18 +0100

    adding a check for argument numbers

--- uptimer.py
+++ uptimer.py
@@ -2,12 +2,15 @@
 import sys
 
 def main():
-    print len(sys.argv)
-    with open(sys.argv[1]) as f:#this should open uptimer-start.log
-        for line in f:
-            print line
-            line = line.strip()
-            print line
+    if(len(sys.argv)==2):
+        with open(sys.argv[1]) as f:#this should open uptimer-start.log
+            for line in f:
+                print line
+                line = line.strip()
+                print line
+    else:
+        print 'Usage: python uptimer.py uptimer-start.log'
+        print len(sys.argv)
 
 if __name__=="__main__":
     main()
